484896464b2efa91bb4d2dffe8781a6ad7137c81
Mostofa Kamal <mostofakamal3636@gmail.com>
2023-01-16 16:16:32 +0600

    show home page

--- header.php
+++ header.php
@@ -1,61 +1,63 @@
 <?php
-$page = basename($_SERVER['PHP_SELF'],".php");
+$page = basename($_SERVER['PHP_SELF'], ".php");
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-      
+
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="#">Blog</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Blog</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item ">
-      <a class="nav-link , <?= ($page == "index")? 'active':''?>" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Categories
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">politics</a>
-          <a class="dropdown-item" href="#">sports</a>
-          
-      </li>
-      <li class="nav-item ">
-        <a class="nav-link , <?= ($page == "login")? 'active':''?>" 
-        href="login.php" >Login </a>
-      </li>
-      <li class="nav-item ">
-        <a class="nav-link , <?= ($page == "Registration")? 'active':''?>" 
-        href="registration.php" >Registration </a>
-      </li>
-      
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
- </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item ">
+            <a class="nav-link , <?= ($page == "index") ? 'active' : '' ?>" href="index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
 
-</nav>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Categories
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">politics</a>
+              <a class="dropdown-item" href="#">sports</a>
 
-  
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link , <?= ($page == "login") ? 'active' : '' ?>" href="login.php">Login </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link , <?= ($page == "Registration") ? 'active' : '' ?>" href="registration.php">Registration </a>
+          </li>
+
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+
+  </nav>
--- index.php
+++ index.php
@@ -1,8 +1,68 @@
-<?php 
-include 'header.php';
+<?php include 'header.php'; 
+include 'Config.php';
+$sql = "SELECT * FROM  blog LEFT JOIN category ON blog.category=category.cat_id 
+ LEFT JOIN user ON blog.author_id = user.user_id";
+ $query = mysqli_query($config , $sql);
+ $row = mysqli_num_rows($query); 
 
 ?>
+<div class="container mt-2">
+	<div class="row">
+		<div class="col-lg-8">
+            <?php 
+              if ($row) {
+                while ($result = mysqli_fetch_assoc($query)) {
+                
+                ?>  
 
-<?php 
-include 'footer.php';
-?>
+			<div class="card shadow">
+				<div class="card-body d-flex blog_flex">
+					<div class="flex-part1">
+						<a href="">
+                            <?php
+                             $img = $result['blog_image'];
+                            ?>
+                             <img src="admin\upload/<?= $img?>"> 
+                            
+                        </a>
+					</div>
+					<div class="flex-grow-1 flex-part2">
+						  <a href="" id="title">
+                            <?= $result['blog_title']?>
+                          </a>
+						<p>
+						  <a href="" id="body">
+                          <?= $result['blog_body']?>
+						  </a> <span><br>
+                          <a href="" class="btn btn-sm btn-outline-primary">Continue Reading
+                          </a></span>
+                        </p>
+						<ul>
+							<li class="me-2 "><a href=""> <span>
+                                <i class="fa fa-pencil-square-o " aria-hidden="true"></i></span>
+                                <?= $result['username']?></a>
+							</li>
+							<li class="me-2">
+								<a href=""> <span><i class="fa fa-calendar-o" aria-hidden="true"></i></span> <?= $result['publish_date']?> </a>
+							</li>
+							<li>
+								<a href=""> <span><i class="fa fa-tag" aria-hidden="true"></i></span> <?= $result['cat_name']?> </a>
+						    </li>
+						</ul>
+					</div>
+				</div>
+			</div>
+            <?php
+                }
+            }
+            ?>
+
+		</div>
+		
+
+        <?php include 'sidebar.php'?>
+	</div>
+</div>
+
+
+<?php include 'footer.php';?>
